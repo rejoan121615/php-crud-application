20e86072b1caa29b760d3c953da0c49e0733cf13
Mohd Rejoan <wearecrx@gmail.com>
2023-05-30 12:39:04 +0600

    database file created

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php
+
+  $connection = new mysqli('localhost', 'root', '', 'news') or die("got an error");
+  $connection->set_charset('utf8');
+  // print_r($connection);
+
+
+?>
--- signup.php
+++ signup.php
@@ -1,7 +1,9 @@
 <?php 
+  require_once("database.php");
 
-
-
+  // if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])) {
+    
+  // }
 
 ?>
 
@@ -15,10 +17,10 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   </head>
-  <body style="height: 100vh" class=" w-100 d-flex justify-content-center align-items-center ">
+  <body style="height: 100vh" class=" w-100 d-flex flex-column justify-content-center align-items-center ">
     <section class=" w-75 ">
       <div class="container">
-      <form>
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <div class="mb-3">
           <label for="name" class="form-label">Name</label>
           <input type="text" class="form-control" id="name" name="name">
